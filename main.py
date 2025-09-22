89743f1191c901e2edd4142dabbcb20e10083717
Apple <apple@MacBook-Pro-cua-Minh.local>
2025-09-22 19:16:44 +0700

    Updated code with new changes

--- main.py
+++ main.py
@@ -19,68 +19,123 @@ def check_arg(args):
                         help='Enter dataset name. Dataset data must be located in: DATASET_ROOT/DATASET',
                         type=str, required=True)
 
-    # Đường dẫn gốc cho dataset (ở đây là /kaggle/working/customdb)
+    # changed according to mode
     parser.add_argument('-k', '--dataset_root',
                         help='Path to database root',
-                        default='/kaggle/working/customdb',  # Đường dẫn dataset chính
+                        default=os.path.join('data', 'source'),
                         required=False)
 
-    # Các tham số khác vẫn như cũ
-    parser.add_argument('--particles',
+    # only for nuscenes (temporarily deactivated)
+    # only for nuscenes and nuscenes_gan
+    # parser.add_argument('--json_file', help='token files in json for nuscenes', required=False)
+    #
+    # parser.add_argument('-g', '--gan_root',
+    #                     help='Path to modified gan database root',
+    #                     default=os.path.join('data', 'gan'),
+    #                     required=False)
+    parser.add_argument('-p', '--post_fix',
+                        help='Post fix added at the end of the modified gan file (ex.: "fake_B")',
+                        default="", type=str, required=False)
+
+    parser.add_argument('-s', '--sequences',
+                        help='List of sequences comma separated (e.g. for KITTI: data_object/training,data_object/testing).',
+                        default='',
+                        required=False)
+
+    # noise
+    parser.add_argument('-ns', '--noise_scale',
+                        type=float,
+                        default=0.0)
+    parser.add_argument('-nv', '--noise_std',
+                        type=float,
+                        default=0.0)
+    parser.add_argument('-oa', '--opacity_attenuation',
+                        help='Opacity attenuation of the rain layer. Values must be between 0 and 1',
+                        type=float,
+                        default=1.0)
+
+    parser.add_argument('-r', '--particles',
                         help='Path to particles simulations',
-                        default='/kaggle/working/particles',  # Đường dẫn tới particles
+                        default=os.path.join('data', 'particles'),
                         required=False)
 
-    parser.add_argument('--streaks_db',
+    # stays the same everywhere
+    parser.add_argument('-sd', '--streaks_db',
                         help='Path to rain streaks database (Garg and Nayar, 2006)',
-                        default='/kaggle/input/database-rainrender',
+                        default=os.path.join('3rdparty', 'rainstreakdb'),
                         required=False)
 
-    parser.add_argument('--intensity',
+    # if not provided outputs results of every supported intensity
+    parser.add_argument('-i', '--intensity',
                         help='Rain Intensities. List of fall rate comma-separated. E.g.: 1,15,25,50.',
                         type=str,
                         default='25',
                         required=False)
 
-    parser.add_argument('--depth',
+    parser.add_argument('-d', '--depth',
                         help='Path to depths',
-                        default='/kaggle/working/customdb/depth',  # Đường dẫn tới thư mục depth
+                        default=os.path.join('data', 'source'),
+                        required=False)
+
+    # ways to not process all the frames in a sequence
+    parser.add_argument('-fs', '--frame_start',
+                        help='Frame start',
+                        type=int,
+                        default=0)
+    parser.add_argument('-fe', '--frame_end',
+                        help='Frame end',
+                        type=int,
+                        default=None)
+    parser.add_argument('-fst', '--frame_step',
+                        help='Frame step',
+                        type=int,
+                        default=1)
+    parser.add_argument('-ff', '--frames',
+                        type=str,
+                        required=False,
+                        default="")
+
+    parser.add_argument('--conflict_strategy',
+                        help='Strategy to use if output already exists.',
+                        type=str,
+                        choices=['overwrite', 'skip', 'rename_folder'],
+                        default='overwrite',
+                        required=False)
+    parser.add_argument('--rendering_strategy',
+                        help='Rendering strategy',
+                        choices=[None, 'white', 'naive_db'],
+                        type=str,
+                        default=None,
                         required=False)
 
     parser.add_argument('--output',
-                        default='/kaggle/working/output',  # Đường dẫn lưu kết quả đầu ra
+                        default=os.path.join('data', 'output'),
                         help='Where to save the output',
                         required=False)
 
+    parser.add_argument('--save_envmap',
+                        help='Save environment maps, useful for debug purposes. NOTE: envmap are overwritten if they exist, regardless of the conflict strategy.',
+                        action='store_true')
+
+    parser.add_argument('--noverbose',
+                        action='store_true')
+
+    parser.add_argument('--force_particles',
+                        help='Force particles simulator to run even if simulation exist',
+                        action='store_true')
+
     results = parser.parse_args(args)
 
-    # Sử dụng các tham số đã cung cấp, tạo đối tượng results
+    assert not results.force_particles or results.conflict_strategy != "skip", "If particles simulator is forced, cannot skip"
+
     results.verbose = not results.noverbose
-
-    # Cập nhật các đường dẫn liên quan đến ảnh và độ sâu
-    results.images_root = os.path.join(results.dataset_root)  # Đường dẫn tới customdb
-    results.images = {
-        'all': os.path.join(results.dataset_root, 'rgb'),  # Đường dẫn tới thư mục rgb
-    }
-
-    results.depth = {
-        'all': os.path.join(results.dataset_root, 'depth'),  # Đường dẫn tới thư mục depth
-    }
-
-    # Kiểm tra các thư mục 'rgb' và 'depth' có tồn tại không
-    for key in results.images:
-        assert os.path.exists(results.images[key]), f"RGB folder for {key} does not exist."
-        assert os.path.exists(results.depth[key]), f"Depth folder for {key} does not exist."
-
-    # Cập nhật các tham số khác
-    results.texture = os.path.join(results.streaks_db, 'env_light_database', 'size32')
-    results.norm_coeff = os.path.join(results.streaks_db, 'env_light_database', 'txt', 'normalized_env_max.txt')
+    results.texture = '/kaggle/input/database-rainrender/env_light_database/size32'
+    results.norm_coeff = '/kaggle/input/database-rainrender/env_light_database/txt/normalized_env_max.txt'
 
     assert os.path.exists(results.streaks_db), ("rainstreakdb database is missing.", results.streaks_db)
     assert os.path.exists(results.texture), ("rainstreakdb database is not valid. Some files are missing.", results.texture)
     assert os.path.exists(results.norm_coeff), ("rainstreakdb database is not valid. Some files are missing.", results.norm_coeff)
 
-    # Cập nhật các tham số về cường độ mưa
     results.intensity = [int(i) for i in results.intensity.split(",")]
     if results.frames:
         results.frames = [int(i) for i in results.frames.split(",")]
@@ -105,7 +160,7 @@ def check_arg(args):
     # Build weathers to render
     results.weather = np.asarray([{"weather": "rain", "fallrate": i} for i in results.intensity])
 
-    # Kiểm tra các sequence hợp lệ
+    # Check sequences are valid
     print("\nChecking sequences...")
     print(" {} sequences found: {}".format(len(results.sequences), [s for s in results.sequences]))
     for seq in results.sequences:
@@ -122,6 +177,7 @@ def check_arg(args):
 
         if not valid:
             results.sequences = results.sequences[results.sequences != seq]
+            # del results.particles[seq]
             del results.images[seq]
             del results.depth[seq]
             del results.calib[seq]
